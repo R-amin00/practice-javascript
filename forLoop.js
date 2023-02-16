ca6b3dbe4bc757f32227233e0e49cf4647fe1f3d
Ruhul <ruhulamin18753@gmail.com>
2023-02-17 00:18:49 +0600

    first intialized

---
+++ forLoop.js
@@ -0,0 +1,85 @@
+//Task-01: Write the programme to sum those number which are divideable by 3 and 5 and it will be continue upto 100
+
+// for (let i = 1; i <= 50; i++) {
+//     if(i%3==0 && i% 5 == 0){
+//         sum = sum + i;
+//         // document.write(sum);
+//         console.log(sum)
+//     }else{
+//         // console.log("end")
+//     }
+    
+// }
+// console.log(sum);
+
+
+
+// // Task-02: write "ajk amar mon valo nei" 39  times
+// for (let i = 1; i < 40; i++) {
+//     const element = i;
+//     console.log(element)
+//     console.log("ajk amar mon valo nei");
+// }
+
+
+// // Task-03: Display number between 58 to 98
+// for (let i = 58; i <= 98; i++) {
+//     const element = i;
+//     console.log(i);
+    
+// }
+
+
+
+// // Task-04: Show all EVEN NUMBER from 412 to 456
+// for (let i = 412; i <= 456;) {
+//     i = i + 2;
+//     console.log(i)
+// }
+
+
+// // Task-05: Show all ODD NUMBER from 581 to 623
+
+// for (let i = 581; i <= 623;) {
+//     console.log(i)
+//     i = i + 2;
+// }
+
+
+
+// // Task-06: Create an array for all the mobile phones. Display all the elements of the array by using FOR loop
+
+// let phone = ['samsung', 'nokia', 'symphony', 'realme', 'infinix', 'vivo', 'oppo'];
+// for (let i = 0; i < phone.length; i++) {
+//     const showPhone = phone[i];
+//     console.log(showPhone);
+// }
+
+
+
+// // Task-07: Run a loop from 30 to 86. This loop will be stopped if the values get higher than 44
+
+// for(let i = 30; i <= 86; i++) {
+//     console.log(i)
+//     if (i > 43) {
+//         break;
+//     }
+// }
+
+
+
+// // Task-08: Write the price of the books that you have. Display the prices if the prices are lower than 200
+
+// let book1 = 250;
+// let book2 = 200;
+// let book3 = 300;
+// let book4 = 170;
+// if(book1 < book2){
+//     console.log("I haven't much money to buy 'Eloquent JavaScript' !")
+// }else if(book2 < book3){
+//     console.log("I have much money to buy 'A Smarter Way to Learn JavaScript' !")
+// }else if(book3 > book4){
+//     console.log("I haven't so much money to buy 'JavaScript and jQuery' !")
+// }else {
+//     console.log("I have much money to buy 'JavaScript: The Definitive Guide' !")
+// }
---
+++ whileLoop.js
@@ -0,0 +1,78 @@
+// // Task-01: Write the programme to sum those number which are divideable by 3 and 5 and it will be continue upto 100
+
+// let i = 1;
+// let sum = 0;
+// while (i <= 20) {
+//     if (i%2==0 && i%5==0) {
+//         sum = sum + i;
+//         i = i + 1;
+//     }else{
+//        i = i + 1;
+//     }
+// }
+// console.log(sum)
+// // document.write(sum)
+
+
+
+// // Task-02: write "ajk amar mon valo nei" 39  times
+
+// let line = 0;
+// while(line < 40) {
+//     line = line + 1;
+//     console.log(line);
+//     console.log("ajk amar mon valo nei")
+// }
+
+
+// // // Task-03: Display number between 58 to 98
+
+// let num = 58
+// while (num <= 98) {
+//     console.log(num);
+//     num = num + 1;
+// }
+
+
+
+// // Task-04: Show all EVEN NUMBER from 412 to 456
+
+// let num = 412;
+// while(num <= 456) {
+//     console.log(num);
+//     num = num + 2;
+    
+// }
+
+
+// // Task-05: Show all ODD NUMBER from 581 to 623
+
+// let num = 581;
+// while(num <= 623) {
+//     console.log(num);
+//     num = num + 2;
+// }
+
+
+// // Task-06: Create an array for all the mobile phones. Display all the elements of the array by using FOR loop
+
+// let phoneAry = ['samsung', 'nokia', 'symphony', 'realme', 'infinix', 'vivo', 'oppo'];
+// let phone = 0;
+// while(phone < phoneAry.length) {
+//     const phoneName = phoneAry[phone];
+//     console.log(phoneName);
+//     phone = phone + 1;
+// }
+
+
+
+// // Task-07: Run a loop from 30 to 86. This loop will be stopped if the values get higher than 44
+
+let num = 30;
+while(num <= 86) {
+    console.log(num); 
+    if (num > 43) {
+        break;
+    }
+    num = num + 1;
+}
