f049b6e1ee37814ca755df0e7426d09c2727a6d2
Ruhul <ruhulamin18753@gmail.com>
2023-02-17 23:36:33 +0600

    task-02 is completed

--- practice-assignment/function.js
+++ practice-assignment/function.js
@@ -1,4 +1,5 @@
 // // Task-01: Convert Radian to Degree
+
 // function radianToDegree (radian) {
 //     let toDegree = radian * (180 / 3.1416).toFixed(4);
 //     return toDegree;
@@ -6,15 +7,29 @@
 // let degree = radianToDegree(10);
 // console.log("10 Radian =", degree,"degree")
 
+
+
 // // Task-02: Check whether the given file name is JavaScript file or not
-// function isJavaScriptFile (params) {
-    
-// }
-// let file = isJavaScriptFile();
-// console.log(file)
+const files = ['file.json', 'file.css', 'file.html', 'file.js', 'file.scss', 'file.xml'];
+function isJavaScriptFile (files) {
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        let lastIndex = file.lastIndexOf(".");
+        let extension = file.substring(lastIndex + 1);
+        let javaScriptExtention = "js";
+        // console.log(extension)
+        if(extension === javaScriptExtention) {
+            return true;
+        }
+    }
+}
+let file = isJavaScriptFile(files);
+console.log(file)
+
 
 
 // // Task-03: Calculate total oil price when each litre of disel, petrol and octane price are 114, 130 and 135 tk
+
 // function oilPrice(disel, petrol, octane) {
 //     let countPrice = (disel * 6) + (petrol * 8) + (octane * 3);
 //     return countPrice;
@@ -23,9 +38,10 @@
 // console.log(totalPrice);
 
 
-// /* Task-04: You have two cars and 80 employee, one is bus which contains 50 people and 
-//     other is microbus where 11 people can get up. You have to send all the employee at their home by car. 
-//     So you have to send over employees in public transeport and you have to pay 250 taka per every employee.*/
+
+// // /* Task-04: You have two cars and 80 employee, one is bus which contains 50 people and 
+// //     other is microbus where 11 people can get up. You have to send all the employee at their home by car. 
+// //     So you have to send over employees in public transeport and you have to pay 250 taka per every employee.*/
 
 // function publicBusFare(employee) {
 //     let busEmployee = employee - 50;
@@ -37,7 +53,9 @@
 // console.log(fare);
 
 
-// Task-05: If first person's name is similar to 2nd person's friend's name or 1st person's friend's name is similar to 2nd person's name , then they will be best friend and it will retuen Ture otherwise False
+
+// // Task-05: If first person's name is similar to 2nd person's friend's name or 1st person's friend's name is similar to 2nd person's name , then they will be best friend and it will retuen Ture otherwise False
+
 // const personFriend = [
 //     {
 //         id: 1,
@@ -49,7 +67,6 @@
 //         person: 'Rock',
 //         friend: 'Tomy',
 //     }
-
 // ]
 // function isBestFriend(personFriend) {
 //     const firstPersonName = personFriend[0].person;
